bfbceeea802f64a5a6c3234a14a5ccc84c2d1b08
Galindo <christian.galindo@aspentech.com>
2023-04-05 14:45:02 -0600

    feat: stablish mikroORM setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello there");
+import { MikroORM } from "@mikro-orm/postgresql";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import mikroOrmConfig from "./mikro-orm.config";
+import express from "express";
+
+const main = async () => {
+    const orm = await MikroORM.init(mikroOrmConfig);
+    await orm.getMigrator().up();
+    
+    const app = express();
+    app.get("/", (_, res) => {
+        res.send("hello");
+    })
+    app.listen(4000, () => {
+        console.log("server started on localhost:4000");
+    });
+}
+
+main().catch(err => {
+    console.error(err);
+});
