59c4e0dfc508743eb271cc88e42bbdcddf38fbeb
GALINDOC <christian.galindo@aspentech.com>
2023-04-05 18:28:16 -0600

    Added env file and /dist to gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
 import { MikroORM } from "@mikro-orm/postgresql";
 import { __prod__ } from "./constants";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "graphql";
+
 import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
-import express from "express";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
     const orm = await MikroORM.init(mikroOrmConfig);
     await orm.getMigrator().up();
     
     const app = express();
-    app.get("/", (_, res) => {
-        res.send("hello");
+    
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
     })
+    
     app.listen(4000, () => {
         console.log("server started on localhost:4000");
     });
