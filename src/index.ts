aae0381b722b082609ac85bdd3f58a9f2df312be
GALINDOC <christian.galindo@aspentech.com>
2023-05-03 09:13:00 -0600

    first commit

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import { MikroORM } from "@mikro-orm/postgresql";
 import { __prod__ } from "./constants";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
-import { buildSchema } from "graphql";
+import { buildSchema } from "type-graphql";
 
-import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
     const orm = await MikroORM.init(mikroOrmConfig);
@@ -16,10 +16,15 @@ const main = async () => {
     
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [HelloResolver],
+            resolvers: [HelloResolver, PostResolver],
             validate: false
-        })
+        }),
+        context: () => ({ em: orm.em }),
     })
+
+    await apolloServer.start();
+
+    apolloServer.applyMiddleware({ app });
     
     app.listen(4000, () => {
         console.log("server started on localhost:4000");
